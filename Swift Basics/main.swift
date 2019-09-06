2b9aa927befbeca2bdf7edffba9653e459152b35
Ante Plecas <anteplecas@Antes-MacBook-Pro.local>
2019-09-06 13:06:15 -0500

    First commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Ante Plecas on 9/6/19.
+//  Copyright © 2019 Ante Plecas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
